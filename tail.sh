cfa3a7f6ff5eb6edd4b9edca9966ee7100b776c2
Igor Wiedler <igor@travis-ci.org>
2018-02-14 17:29:09 +0100

    a lot of copy pasta for all of the site-infra combinations

--- tail.sh
+++ tail.sh
@@ -6,13 +6,159 @@ rm -f $psmgr
 mkfifo $psmgr
 
 (
-  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_ORG papertrail --group "04 - EC2 Workers" 'program:travis-worker' -f -j | jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | honeytail --writekey=$HONEYCOMB_WRITEKEY --dataset='worker' --parser=keyval --keyval.timefield=time --keyval.filter_regex='time=' --file=- --add_field site=org --add_field infra=ec2 --samplerate 10 --dynsampling level --dynsampling repository
-  echo 'org-ec2' >$psmgr
+  SITE=org
+  INFRA=ec2
+  PAPERTRAIL_GROUP="04 - EC2 Workers"
+
+  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_ORG papertrail \
+      --group "$PAPERTRAIL_GROUP" \
+      'program:travis-worker' \
+      -f -j | \
+    jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | \
+    honeytail \
+      --writekey=$HONEYCOMB_WRITEKEY \
+      --dataset='worker' \
+      --parser=keyval \
+      --keyval.timefield=time \
+      --keyval.filter_regex='time=' \
+      --file=- \
+      --add_field site=$SITE \
+      --add_field infra=$INFRA \
+      --samplerate 10 \
+      --dynsampling level \
+      --dynsampling repository
+
+  echo "$SITE-$INFRA" >$psmgr
 ) &
 
 (
-  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_ORG papertrail --group "05 - GCE Workers" 'program:travis-worker' -f -j | jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | honeytail --writekey=$HONEYCOMB_WRITEKEY --dataset='worker' --parser=keyval --keyval.timefield=time --keyval.filter_regex='time=' --file=- --add_field site=org --add_field infra=gce --samplerate 10 --dynsampling level --dynsampling repository
-  echo 'org-gce' >$psmgr
+  SITE=org
+  INFRA=gce
+  PAPERTRAIL_GROUP="05 - GCE Workers"
+
+  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_ORG papertrail \
+      --group "$PAPERTRAIL_GROUP" \
+      'program:travis-worker' \
+      -f -j | \
+    jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | \
+    honeytail \
+      --writekey=$HONEYCOMB_WRITEKEY \
+      --dataset='worker' \
+      --parser=keyval \
+      --keyval.timefield=time \
+      --keyval.filter_regex='time=' \
+      --file=- \
+      --add_field site=$SITE \
+      --add_field infra=$INFRA \
+      --samplerate 10 \
+      --dynsampling level \
+      --dynsampling repository
+
+  echo "$SITE-$INFRA" >$psmgr
+) &
+
+(
+  SITE=org
+  INFRA=macstadium
+  PAPERTRAIL_GROUP="08 - MacStadium"
+
+  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_ORG papertrail \
+      --group "$PAPERTRAIL_GROUP" \
+      'program:travis-worker' \
+      -f -j | \
+    jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | \
+    honeytail \
+      --writekey=$HONEYCOMB_WRITEKEY \
+      --dataset='worker' \
+      --parser=keyval \
+      --keyval.timefield=time \
+      --keyval.filter_regex='time=' \
+      --file=- \
+      --add_field site=$SITE \
+      --add_field infra=$INFRA \
+      --samplerate 10 \
+      --dynsampling level \
+      --dynsampling repository
+
+  echo "$SITE-$INFRA" >$psmgr
+) &
+
+(
+  SITE=com
+  INFRA=ec2
+  PAPERTRAIL_GROUP="04 - EC2 Workers"
+
+  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_COM papertrail \
+      --group "$PAPERTRAIL_GROUP" \
+      'program:travis-worker' \
+      -f -j | \
+    jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | \
+    honeytail \
+      --writekey=$HONEYCOMB_WRITEKEY \
+      --dataset='worker' \
+      --parser=keyval \
+      --keyval.timefield=time \
+      --keyval.filter_regex='time=' \
+      --file=- \
+      --add_field site=$SITE \
+      --add_field infra=$INFRA \
+      --samplerate 10 \
+      --dynsampling level \
+      --dynsampling repository
+
+  echo "$SITE-$INFRA" >$psmgr
+) &
+
+(
+  SITE=com
+  INFRA=gce
+  PAPERTRAIL_GROUP="05 - GCE Workers"
+
+  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_COM papertrail \
+      --group "$PAPERTRAIL_GROUP" \
+      'program:travis-worker' \
+      -f -j | \
+    jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | \
+    honeytail \
+      --writekey=$HONEYCOMB_WRITEKEY \
+      --dataset='worker' \
+      --parser=keyval \
+      --keyval.timefield=time \
+      --keyval.filter_regex='time=' \
+      --file=- \
+      --add_field site=$SITE \
+      --add_field infra=$INFRA \
+      --samplerate 10 \
+      --dynsampling level \
+      --dynsampling repository
+
+  echo "$SITE-$INFRA" >$psmgr
+) &
+
+(
+  SITE=com
+  INFRA=macstadium
+  PAPERTRAIL_GROUP="08 - MacStadium"
+
+  PAPERTRAIL_API_TOKEN=$PAPERTRAIL_API_TOKEN_COM papertrail \
+      --group "$PAPERTRAIL_GROUP" \
+      'program:travis-worker' \
+      -f -j | \
+    jq -cr '.events[]|"hostname=" + .hostname + " " + .message' | \
+    honeytail \
+      --writekey=$HONEYCOMB_WRITEKEY \
+      --dataset='worker' \
+      --parser=keyval \
+      --keyval.timefield=time \
+      --keyval.filter_regex='time=' \
+      --file=- \
+      --add_field site=$SITE \
+      --add_field infra=$INFRA \
+      --samplerate 10 \
+      --dynsampling level \
+      --dynsampling repository
+
+  echo "$SITE-$INFRA" >$psmgr
 ) &
 
 read exit_process <$psmgr
